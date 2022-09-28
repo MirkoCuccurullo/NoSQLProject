a14868859cea7ba783c5435696ba3e1123e52770
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2022-09-28 13:23:52 +0200

    added UI

--- UI/Program.cs
+++ UI/Program.cs
@@ -15,8 +15,9 @@ namespace DemoApp
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(true);
             Application.Run(new Form1());
+
         }
     }
 }
