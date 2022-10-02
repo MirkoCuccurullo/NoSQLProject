75b23c393b5d02b6b7f9ea43c8f3caac3c3d159f
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2022-10-02 17:05:25 +0200

    changed enumeration <ticketDeadline> indexes

--- Model/Enums.cs
+++ Model/Enums.cs
@@ -8,7 +8,7 @@ namespace Model
 {
     public enum TicketType { Service, Software, Hardware }
     public enum TicketPriority { Low, Medium, High }
-    public enum TicketDeadline { Seven, Fourtheen, Twentysix }
+    public enum TicketDeadline { Seven = 7, Fourteen = 14, Twentysix = 26 }
 
     public enum PanelName { CreateTicket, Dashboard}
 
