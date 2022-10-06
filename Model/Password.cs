d87aa6ffd20754427c721ad3e8a39e397c0e33d3
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2022-10-06 11:18:47 +0200

    created Login form

---
+++ Model/Password.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Password
+    {
+        [BsonElement("hash")]
+        public string Hash { get; set; }
+        [BsonElement("salt")]
+        public string Salt { get; set; }
+
+        [BsonElement("question")]
+        public string Question { get; set; }
+        [BsonElement("answer")]
+        public string Answer { get; set; }
+    }
+}
