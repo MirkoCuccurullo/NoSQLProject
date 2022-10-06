d7caad6731d4cf5c06ab6a26378eb0c3ea14e363
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2022-10-06 10:51:14 +0200

    Squashed commit of the following:
    commit 8968bbabc0c422d5a87a0b66766372f8dfd3e912
    Author: MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
    Date:   Thu Oct 6 10:45:25 2022 +0200
    
        Squashed commit of the following:
    
        commit c685cbc1edc08f3e141a89edec7499590b7497a0
        Author: bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
        Date:   Thu Oct 6 10:42:15 2022 +0200
    
            changed made to work for creating User
    
        commit 974d36d37a750c84f8b7925519bc894ea11423ee
        Merge: 749f0a0 75b23c3
        Author: MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
        Date:   Tue Oct 4 09:24:57 2022 +0200
    
            Merge remote-tracking branch 'origin/Mirko' into Bijay
    
        commit 749f0a0020173bbcdfc291088bbdac28abe320cf
        Merge: 5bf3848 028edff
        Author: bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
        Date:   Mon Oct 3 22:14:36 2022 +0200
    
            Merge pull request #2 from MirkoCuccurullo/Bijay
    
            Merge pull request #1 from MirkoCuccurullo/master
    
        commit 028edfff2f59d4fa63468d0201e95e8b8d56d22f
        Merge: 8468445 5bf3848
        Author: bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
        Date:   Mon Oct 3 22:13:27 2022 +0200
    
            Merge pull request #1 from MirkoCuccurullo/master
    
            just
    
    commit 1a237980f5faf5b84b4a506435aa652d710becea
    Author: MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
    Date:   Tue Oct 4 10:31:51 2022 +0200
    
        added ticket overview panel and ticket DAO

---
+++ DAL/TicketDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace DAL
+{
+    public class TicketDAO : DAO
+    {
+
+        public List<Ticket> GetAllTickets()
+        {
+            List<Ticket> users = new List<Ticket>();
+
+            //getting user collection
+            var collection = base.ReturnCollection("Ticket");
+
+            //getting all documents in the collection 
+            var documents = collection.Find(new BsonDocument()).ToList();
+
+            foreach (BsonDocument document in documents)
+            {
+                //deserealizing a Bdon document in a User object
+                Ticket user = BsonSerializer.Deserialize<Ticket>(document);
+                users.Add(user);
+            }
+
+            return users;
+        }
+    }
+}
